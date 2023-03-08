8d5d57411d8501c2b0f925d01e2c44f3d221f956
kleber kunha <kleberkunha@hotmail.com>
2023-03-08 20:24:38 +0100

    Sending changes in the search bar from HOme

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all.reverse
+    @q = User.ransack(params[:q])
+
+    @users = @q.result(distinct: true)
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
+  include PgSearch::Model
   primary_abstract_class
+
+  scope :sorted, -> { order(first_name: :asc) }
+
+  pg_search_scope :global_search,
+                  against: %i[first_name last_name],
+                  using: {
+                    tsearch: {
+                      prefix: true
+                    }
+                  }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["first_name"]
+  end
 end
